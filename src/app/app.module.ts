a4f883a5d2e1598b6b3401efe773a1b02d963f75
Christian Smith Mantas <casm202@gmail.com>
2023-07-25 17:32:16 +0200

    Update: add basic component templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeroSectionComponent } from './components/hero-section/hero-section.component';
+import { CardComponent } from './components/card/card.component';
+import { CategoryComponent } from './components/category/category.component';
+import { ButtonComponent } from './components/button/button.component';
+import { MediaCardComponent } from './components/media-card/media-card.component';
+import { SliderComponent } from './components/slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroSectionComponent,
+    CardComponent,
+    CategoryComponent,
+    ButtonComponent,
+    MediaCardComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
